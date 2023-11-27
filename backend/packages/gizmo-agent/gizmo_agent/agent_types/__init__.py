72014c3c452b1d84fe8505f8652f349023bf95d0
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2023-11-27 20:39:18 +0000

    Update __init__.py

--- backend/packages/gizmo-agent/gizmo_agent/agent_types/__init__.py
+++ backend/packages/gizmo-agent/gizmo_agent/agent_types/__init__.py
@@ -8,12 +8,12 @@ class GizmoAgentType(str, Enum):
     GPT_35_TURBO = "GPT 3.5 Turbo"
     # GPT_4 = "GPT 4"
     AZURE_OPENAI = "GPT 4 (Azure OpenAI)"
-    CLAUDE2 = "Claude 2"
+    CLAUDE2 = "test"
     BEDROCK_CLAUDE2 = "Claude 2 (Amazon Bedrock)"
 
 
 __all__ = [
     "get_openai_function_agent",
-    "get_xml_agent",
+    "test",
     "GizmoAgentType",
 ]
